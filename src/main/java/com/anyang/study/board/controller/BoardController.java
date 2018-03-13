92bf30c4fab54fef4b28278f92eea599a8dd5919
xbb123 <xbb123@naver.com>
2018-03-14 00:53:20 +0900

    보드 서비스 추가. 삭제기능 추가 Autowired 리뷰 검토해야

--- src/main/java/com/anyang/study/board/controller/BoardController.java
+++ src/main/java/com/anyang/study/board/controller/BoardController.java
@@ -1,6 +1,9 @@
 package com.anyang.study.board.controller;
 
+import com.anyang.study.board.domain.Board;
+import com.anyang.study.board.service.BoardServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,6 +11,9 @@ import org.springframework.web.servlet.ModelAndView;
 @Slf4j
 @Controller
 public class BoardController {
+
+    @Autowired
+    private BoardServiceImpl boardService;
 
     @RequestMapping("/board")
     public ModelAndView hello() {
@@ -20,5 +26,20 @@ public class BoardController {
         return mav;
     }
 
+    @RequestMapping("/board/delete")
+    public ModelAndView delete() {
+        log.info("board delete");
+        Board board = new Board();
+        Long id;
+        id = 12L;
+
+        board.setId(id);
+        boardService.deleteBoard(board);
+        ModelAndView mav = new ModelAndView("board");
+        mav.addObject("Title", "보드게시판");
+        mav.addObject("Content", "게시판 삭제되었습니다.");
+
+        return mav;
+    }
 
 }
