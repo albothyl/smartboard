98ce5645686319e829c4dad28a394816aa7332fa
daehoanshin <xbb123@naver.com>
2018-03-23 02:32:09 +0900

    update 삭제.

--- src/main/java/com/anyang/study/board/application/BoardService.java
+++ src/main/java/com/anyang/study/board/application/BoardService.java
@@ -14,9 +14,6 @@ public interface BoardService {
     //게시판 정보를 화면에서 입력하여 항목의 정합성을 체크하고 데이터베이스에 저장한다.
     Board insertBoard(Board board);
 
-    //화면에 조회된 게시 정보를 수정하여 항목의 정합성을 체크하고 수정된 데이터를 데이터베이스에 반영한다.
-    void updateBoard(Board board);
-
     //선택한 게시판 정보를 삭제한다.
     void deleteBoard(Board board);
 
